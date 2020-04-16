c1f1867afc4fde5a26a5b6043bb7f41996c28499
Alexander <alexander@recording-laptop>
2020-04-16 23:07:24 +0200

    Add CPU usage threshold

--- main.cpp
+++ main.cpp
@@ -12,8 +12,12 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+/*
+The CPU usage stuff was copied from https://github.com/vivaladav/BitsOfBytes/tree/master/cpp-program-to-get-cpu-usage-from-command-line-in-linux
+*/
 
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <vector>
 
@@ -22,13 +26,141 @@
 
 #include <tclap/CmdLine.h>
 
+const int NUM_CPU_STATES = 10;
 
-int getloadavg(double loadavg[], int nelem);
+enum CPUStates
+{
+    S_USER = 0,
+    S_NICE,
+    S_SYSTEM,
+    S_IDLE,
+    S_IOWAIT,
+    S_IRQ,
+    S_SOFTIRQ,
+    S_STEAL,
+    S_GUEST,
+    S_GUEST_NICE
+};
+
+typedef struct CPUData
+{
+    std::string cpu;
+    size_t times[NUM_CPU_STATES];
+} CPUData;
+
+void ReadStatsCPU(std::vector<CPUData> & entries) {
+    std::ifstream fileStat("/proc/stat");
+
+    std::string line;
+
+    const std::string STR_CPU("cpu");
+    const std::size_t LEN_STR_CPU = STR_CPU.size();
+    const std::string STR_TOT("tot");
+
+    while(std::getline(fileStat, line))
+    {
+        // cpu stats line found
+        if(!line.compare(0, LEN_STR_CPU, STR_CPU))
+        {
+            std::istringstream ss(line);
+
+            // store entry
+            entries.emplace_back(CPUData());
+            CPUData & entry = entries.back();
+
+            // read cpu label
+            ss >> entry.cpu;
+
+            // remove "cpu" from the label when it's a processor number
+            if(entry.cpu.size() > LEN_STR_CPU)
+                entry.cpu.erase(0, LEN_STR_CPU);
+            // replace "cpu" with "tot" when it's total values
+            else
+                entry.cpu = STR_TOT;
+
+            // read times
+            for(int i = 0; i < NUM_CPU_STATES; ++i)
+                ss >> entry.times[i];
+        }
+    }
+}
+
+size_t GetIdleTime(const CPUData & e)
+{
+    return	e.times[S_IDLE] +
+            e.times[S_IOWAIT];
+}
+
+size_t GetActiveTime(const CPUData & e)
+{
+    return	e.times[S_USER] +
+            e.times[S_NICE] +
+            e.times[S_SYSTEM] +
+            e.times[S_IRQ] +
+            e.times[S_SOFTIRQ] +
+            e.times[S_STEAL] +
+            e.times[S_GUEST] +
+            e.times[S_GUEST_NICE];
+}
+
+double PrintStats(const std::vector<CPUData> & entries1, const std::vector<CPUData> & entries2)
+{
+    const size_t NUM_ENTRIES = entries1.size();
+    double total_cpu_usage = 100;
+
+    for(size_t i = 0; i < NUM_ENTRIES; ++i)
+    {
+        const CPUData & e1 = entries1[i];
+        const CPUData & e2 = entries2[i];
+
+        std::cout.width(3);
+        std::cout << e1.cpu << "] ";
+
+        const float ACTIVE_TIME	= static_cast<float>(GetActiveTime(e2) - GetActiveTime(e1));
+        const float IDLE_TIME	= static_cast<float>(GetIdleTime(e2) - GetIdleTime(e1));
+        const float TOTAL_TIME	= ACTIVE_TIME + IDLE_TIME;
+
+        std::cout << "active: ";
+        std::cout.setf(std::ios::fixed, std::ios::floatfield);
+        std::cout.width(6);
+        std::cout.precision(2);
+        std::cout << (100.f * ACTIVE_TIME / TOTAL_TIME) << "%";
+        if ("tot" == e1.cpu) {
+            total_cpu_usage = (100.f * ACTIVE_TIME / TOTAL_TIME);
+        }
+
+        std::cout << " - idle: ";
+        std::cout.setf(std::ios::fixed, std::ios::floatfield);
+        std::cout.width(6);
+        std::cout.precision(2);
+        std::cout << (100.f * IDLE_TIME / TOTAL_TIME) << "%" << std::endl;
+    }
+
+    return total_cpu_usage;
+}
+
+double getCPU_Usage() {
+    std::vector<CPUData> entries1;
+    std::vector<CPUData> entries2;
+
+    // snapshot 1
+    ReadStatsCPU(entries1);
+
+    // 100ms pause
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    // snapshot 2
+    ReadStatsCPU(entries2);
+
+    // print output
+    return PrintStats(entries1, entries2);
+}
 
 
 int main(int argc, char ** argv) {
 
     double max_load = std::thread::hardware_concurrency();
+    double max_cpu_usage = 90;
 
     try {
         TCLAP::CmdLine cmd("tool for running programs if enough resources are available", ' ', "0.1");
@@ -40,6 +172,13 @@ int main(int argc, char ** argv) {
                                                  false, std::thread::hardware_concurrency(), "maximum load");
         cmd.add(max_load_arg);
 
+
+        TCLAP::ValueArg<double> max_cpu_arg("c", "cpu-max",
+                                                 "Maximum CPU usage [0-100]. "
+                                                 "If the 1-second cpu usage average is above this threshold "
+                                                 "no new processes are started.",
+                                                 false, 90, "maximum cpu usage");
+        cmd.add(max_cpu_arg);
 
 
         cmd.parse(argc, argv);
@@ -72,6 +211,7 @@ int main(int argc, char ** argv) {
 
     while (true) {
         bool start_next = true;
+        double const total_cpu_usage = getCPU_Usage();
 
         double load = 0;
         int const load_success = getloadavg(&load, 1);
@@ -85,6 +225,16 @@ int main(int argc, char ** argv) {
         else {
             std::cout << "Load is " << load << " which is below the threshold of " << max_load << "." << std::endl;
         }
+
+        if (total_cpu_usage > max_cpu_usage) {
+            std::cout << "CPU usage is " << total_cpu_usage << " which is above the threshold of " << max_cpu_usage << "." << std::endl;
+            start_next = false;
+        }
+        else {
+            std::cout << "CPU usage is " << total_cpu_usage << " which is below the threshold of " << max_cpu_usage << "." << std::endl;
+        }
+
+
 
         if (start_next) {
             std::cout << "starting command:" << std::endl
