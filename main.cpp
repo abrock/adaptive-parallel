d39eb9620eaa7726c1202272f2c46a82fd068c32
Alexander <alexander@recording-laptop>
2020-04-19 18:36:04 +0200

    Less verbose, starting jobs faster, --jobs argument

--- main.cpp
+++ main.cpp
@@ -111,29 +111,29 @@ double PrintStats(const std::vector<CPUData> & entries1, const std::vector<CPUDa
     for(size_t i = 0; i < NUM_ENTRIES; ++i)
     {
         const CPUData & e1 = entries1[i];
-        const CPUData & e2 = entries2[i];
-
-        std::cout.width(3);
-        std::cout << e1.cpu << "] ";
-
-        const float ACTIVE_TIME	= static_cast<float>(GetActiveTime(e2) - GetActiveTime(e1));
-        const float IDLE_TIME	= static_cast<float>(GetIdleTime(e2) - GetIdleTime(e1));
-        const float TOTAL_TIME	= ACTIVE_TIME + IDLE_TIME;
-
-        std::cout << "active: ";
-        std::cout.setf(std::ios::fixed, std::ios::floatfield);
-        std::cout.width(6);
-        std::cout.precision(2);
-        std::cout << (100.f * ACTIVE_TIME / TOTAL_TIME) << "%";
         if ("tot" == e1.cpu) {
-            total_cpu_usage = (100.f * ACTIVE_TIME / TOTAL_TIME);
-        }
+            const CPUData & e2 = entries2[i];
 
-        std::cout << " - idle: ";
-        std::cout.setf(std::ios::fixed, std::ios::floatfield);
-        std::cout.width(6);
-        std::cout.precision(2);
-        std::cout << (100.f * IDLE_TIME / TOTAL_TIME) << "%" << std::endl;
+            std::cout.width(3);
+            std::cout << e1.cpu << "] ";
+
+            const float ACTIVE_TIME	= static_cast<float>(GetActiveTime(e2) - GetActiveTime(e1));
+            const float IDLE_TIME	= static_cast<float>(GetIdleTime(e2) - GetIdleTime(e1));
+            const float TOTAL_TIME	= ACTIVE_TIME + IDLE_TIME;
+
+            std::cout << "active: ";
+            std::cout.setf(std::ios::fixed, std::ios::floatfield);
+            std::cout.width(6);
+            std::cout.precision(2);
+            std::cout << (100.f * ACTIVE_TIME / TOTAL_TIME) << "%";
+            total_cpu_usage = (100.f * ACTIVE_TIME / TOTAL_TIME);
+
+            std::cout << " - idle: ";
+            std::cout.setf(std::ios::fixed, std::ios::floatfield);
+            std::cout.width(6);
+            std::cout.precision(2);
+            std::cout << (100.f * IDLE_TIME / TOTAL_TIME) << "%" << std::endl;
+        }
     }
 
     return total_cpu_usage;
@@ -147,7 +147,7 @@ double getCPU_Usage() {
     ReadStatsCPU(entries1);
 
     // 100ms pause
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
 
     // snapshot 2
     ReadStatsCPU(entries2);
@@ -156,45 +156,80 @@ double getCPU_Usage() {
     return PrintStats(entries1, entries2);
 }
 
+static std::vector<std::thread *> threads;
+static std::vector<uint8_t> finished_threads;
+
+size_t num_running_threads() {
+    size_t result = 0;
+    for (const auto finished : finished_threads) {
+        if (!finished) {
+            result++;
+        }
+    }
+    return result;
+}
+
+void start_thread(std::string const cmd) {
+    size_t const index = finished_threads.size();
+    std::cout << "starting command # " << index << ":" << std::endl
+              << cmd << std::endl;
+    finished_threads.push_back(false);
+    system(cmd.c_str());
+    finished_threads[index] = true;
+    std::cout << "finished command # " << index << ":" << std::endl
+              << cmd << std::endl;
+}
+
 
 int main(int argc, char ** argv) {
 
     double max_load = std::thread::hardware_concurrency();
+    int max_jobs = std::thread::hardware_concurrency();
+
     double max_cpu_usage = 90;
+
+    int normal_delay = 1;
+    int started_delay = 0;
 
     try {
         TCLAP::CmdLine cmd("tool for running programs if enough resources are available", ' ', "0.1");
 
         TCLAP::ValueArg<double> max_load_arg("l", "load-max",
-                                                 "Maximum 1-minute load average. "
-                                                 "If the 1-minute load average is above this threshold "
-                                                 "no new processes are started.",
-                                                 false, std::thread::hardware_concurrency(), "maximum load");
+                                             "Maximum 1-minute load average. "
+                                             "If the 1-minute load average is above this threshold "
+                                             "no new processes are started.",
+                                             false, std::thread::hardware_concurrency(), "maximum load");
         cmd.add(max_load_arg);
+
+        TCLAP::ValueArg<int> max_jobs_arg("j", "jobs",
+                                             "Maximum number of parallel processes started by this program. "
+                                             "As long as the active processes started by this program is larger or equal, "
+                                             "no new processes are started.",
+                                             false, std::thread::hardware_concurrency(), "maximum load");
+        cmd.add(max_jobs_arg);
 
 
         TCLAP::ValueArg<double> max_cpu_arg("c", "cpu-max",
-                                                 "Maximum CPU usage [0-100]. "
-                                                 "If the 1-second cpu usage average is above this threshold "
-                                                 "no new processes are started.",
-                                                 false, 90, "maximum cpu usage");
+                                            "Maximum CPU usage [0-100]. "
+                                            "If the 1-second cpu usage average is above this threshold "
+                                            "no new processes are started.",
+                                            false, 97, "maximum cpu usage");
         cmd.add(max_cpu_arg);
 
 
         cmd.parse(argc, argv);
 
 
-        max_load = max_load_arg.getValue();
+        max_load = std::max(0.5, max_load_arg.getValue());
 
-        if (max_load <= 0) {
-            max_load = .5;
-        }
+        max_jobs = std::max(1, max_jobs_arg.getValue());
 
         max_cpu_usage = std::min(100.0, std::max(1.0, max_cpu_arg.getValue()));
 
         std::cout << "Parameters: " << std::endl
                   << "max. load:      " << max_load << std::endl
-                  << "max. cpu usage: " << max_cpu_usage << std::endl;
+                  << "max. cpu usage: " << max_cpu_usage << std::endl
+                  << "max. jobs:      " << max_jobs << std::endl;
     }
     catch (TCLAP::ArgException const & e) {
         std::cerr << e.what() << std::endl;
@@ -204,8 +239,6 @@ int main(int argc, char ** argv) {
         std::cerr << "Unknown exception" << std::endl;
         return 0;
     }
-
-    std::vector<std::thread *> threads;
 
     std::string cmd;
     if (!(std::getline(std::cin, cmd))) {
@@ -218,6 +251,8 @@ int main(int argc, char ** argv) {
 
         double load = 0;
         int const load_success = getloadavg(&load, 1);
+
+        size_t const num_jobs = num_running_threads();
         if (load_success < 1) {
             std::cerr << "Warning: getloadavg failed." << std::endl;
         }
@@ -237,17 +272,23 @@ int main(int argc, char ** argv) {
             std::cout << "CPU usage is " << total_cpu_usage << " which is below the threshold of " << max_cpu_usage << "." << std::endl;
         }
 
+        if (num_jobs >= max_jobs) {
+            std::cout << "#jobs is " << num_jobs << " which is above the threshold of " << max_jobs << "." << std::endl;
+            start_next = false;
+        }
+        else {
+            std::cout << "#jobs is " << num_jobs << " which is below the threshold of " << max_jobs << "." << std::endl;
+        }
+
 
 
         if (start_next) {
-            std::cout << "starting command:" << std::endl
-                      << cmd << std::endl;
-            std::thread * t = new std::thread(system, cmd.c_str());
+            std::thread * t = new std::thread(start_thread, cmd);
             threads.push_back(t);
             if (!(std::getline(std::cin, cmd))) {
                 break;
             }
-            sleep(30);
+            sleep(started_delay);
         }
         else {
             std::cout << "not starting command:" << std::endl
@@ -255,8 +296,6 @@ int main(int argc, char ** argv) {
         }
 
         std::cout << std::endl;
-
-        sleep(10);
     }
 
     for (std::thread * t : threads) {
